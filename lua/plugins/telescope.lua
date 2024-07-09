fd20dfa52a4cceefbbe70a87ce4d926d1793b0c1
Tommy Dronkers <tommy.dronkers@volvocars.com>
2024-07-09 09:15:14 +0200

    added search in all files command

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,9 @@
 local utils = require('utils')
 
-require('telescope').setup{
-    defaults = {
-    }
+require('telescope').setup {
+  defaults = {
+  }
 }
 
 utils.map('n', '<C-p>', ':Telescope find_files<CR>')
+utils.map('n', '<C-f>', ':Telescope live_grep<CR>')
