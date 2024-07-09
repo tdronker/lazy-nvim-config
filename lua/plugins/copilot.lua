d4e267e59ab16d574c3e830a332c42b13c108503
Tommy Dronkers <tommy.dronkers@volvocars.com>
2024-07-09 12:49:30 +0200

    added C-c for opening copilotchat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,15 +2,17 @@ local utils = require('utils')
 
 require('CopilotChat').setup({
 })
-utils.map('n', '<C-i>', ':CopilotChat<CR>', { noremap = true, silent = true })
+utils.map('n', '<C-c>', ':CopilotChat<CR>', { noremap = true, silent = true })
+utils.map('v', '<C-c>', ':CopilotChat<CR>', { noremap = true, silent = true })
+
 
 require('copilot').setup({
-    suggestion = { enabled = false },
-    cmp = {
-        enabled = true,
-        method = "getCompletionsCycling",
-    },
-    panel = {
-        enabled = false,
-    },
+  suggestion = { enabled = false },
+  cmp = {
+    enabled = true,
+    method = "getCompletionsCycling",
+  },
+  panel = {
+    enabled = false,
+  },
 })
