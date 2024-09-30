f507ff22092827b1a747d6ae570290c20c797d6b
Tommy Dronkers <tommy.dronkers@volvocars.com>
2024-09-30 12:51:54 +0200

    changed keybinding for bufferpick, to leader leader

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,6 +1,6 @@
 local utils = require("utils")
 
-utils.map("n", "<leader>b", ":BufferPick<CR>", { noremap = true, silent = true })
+utils.map("n", "<leader><leader>", ":BufferPick<CR>", { noremap = true, silent = true })
 utils.map("n", "<Tab>", ":BufferNext<CR>", { noremap = true, silent = true })
 utils.map("n", "<S-Tab>", ":BufferPrev<CR>", { noremap = true, silent = true })
 utils.map("n", "<leader>xx", ":BufferCloseAllButVisible<CR>", { noremap = true, silent = true })
